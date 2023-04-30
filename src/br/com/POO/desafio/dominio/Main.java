abca5be5f18e3a8a5d61cb7cfda08f4d9e2f1831
Arthur Aloise <arthur.aloise@gmail.com>
2023-04-29 21:11:42 -0400

    Mentoria e Curso

---
+++ src/br/com/POO/desafio/dominio/Main.java
@@ -0,0 +1,23 @@
+package br.com.POO.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args){
+
+        Curso curso = new Curso();
+        curso.setTitulo("Java Avdvanced!");
+        curso.setDescricao("Cuso completo");
+        curso.setCargaHoraria(8);
+
+        System.out.println(curso);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Duvidas Java");
+        mentoria.setDescricao("Dúvidas com POO");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+
+    }
+}
